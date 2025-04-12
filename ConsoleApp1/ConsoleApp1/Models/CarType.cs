abc6571a6a90bc1da6baf4df88759f64912ae19f
del-luna-7 <7del.luna2008@gmail.com>
2025-04-12 11:39:18 +0400

    Delete ConsoleApp1 directory

--- ConsoleApp1/ConsoleApp1/Models/CarType.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ConsoleApp1.Models;
-
-public class CarType
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-}
--- ConsoleApp1/ConsoleApp1/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleApp1.Models;
-
-public class Employee
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string Position { get; set; } = null!;
-    public List<Sale> Sales { get; set; } = new();
-}
