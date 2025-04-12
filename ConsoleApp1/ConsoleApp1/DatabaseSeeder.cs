6a8b3b9b9912a709ea5f1244d6f43a7ca29d83e6
del-luna-7 <7del.luna2008@gmail.com>
2025-04-12 11:38:08 +0400

    Add files via upload
    EF core lesson 1

---
+++ ConsoleApp1/ConsoleApp1/DatabaseSeeder.cs
@@ -0,0 +1,52 @@
+﻿using CodeFirst.Data.Contexts;
+using ConsoleApp1.Models;
+
+namespace ConsoleApp1;
+
+public class DatabaseSeeder
+{
+    public static void Seed(ShowroomContext context)
+    {
+        if (!context.CarTypes.Any())
+        {
+            context.CarTypes.AddRange(
+                new CarType{ Name = "Седан"},
+                new CarType{ Name = "Внедорожник"}
+                );
+            context.SaveChanges();
+        }
+        
+        if (!context.Cars.Any())
+        {
+            context.Cars.AddRange(
+                new Car{ Make = "Mitsubishi", Model = "Lancer", CarTypeId = 1, Year = 2020, Price = 50000},
+                new Car{ Make = "Toyota", Model = "Cruiser", CarTypeId = 2, Year = 2024, Price = 70000}
+            );
+            context.SaveChanges();
+        }
+        
+        if (!context.Customers.Any())
+        {
+            context.Customers.AddRange(
+                new Customer{ Name = "Huseyn", Email = "gusain208@gmail.com", Phone = "12345678" }
+            );
+            context.SaveChanges();
+        }
+        
+        if (!context.Employees.Any())
+        {
+            context.Employees.AddRange(
+                new Employee{ Name = "Rustam", Position = "Sells Manager"}
+            );
+            context.SaveChanges();
+        }
+        
+        if (!context.Sales.Any())
+        {
+            context.Sales.AddRange(
+                new Sale{ CarId = 1, CustomerId = 1, EmployeeId = 1, SaleDate = DateTime.Now, FinalPrice = 50000}
+            );
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/LINQ.cs
@@ -0,0 +1,24 @@
+﻿using CodeFirst.Data.Contexts;
+using ConsoleApp1.Models;
+
+namespace ConsoleApp1;
+
+public class LINQ
+{
+    private readonly ShowroomContext _context;
+
+    public LINQ(ShowroomContext context)
+    {
+        _context = context;
+    }
+
+    public List<Car> GetCars(int customerId)
+    {
+        return _context.Sales.Where(x => x.CustomerId == customerId).Select(x => x.Car).ToList();
+    }
+
+    public List<Sale> GetSales(DateTime startDate, DateTime endDate)
+    {
+        return _context.Sales.Where(x => x.SaleDate >= startDate && x.SaleDate <= endDate).ToList();
+    }
+}
